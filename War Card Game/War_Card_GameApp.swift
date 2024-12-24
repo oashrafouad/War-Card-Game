8425c339f28a240e601ccb1dbe3b641daa98a73f
Omar Ashraf <oashrafouad@gmail.com>
2024-12-25 00:56:27 +0200

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Omar Ashraf on 25/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
